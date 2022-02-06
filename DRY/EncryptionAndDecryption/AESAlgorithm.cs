64e727a89d4fce31bbaf0eed95b540e998cdbaf7
Emir TARTAR <emirtartar35@gmail.com>
2022-02-06 12:21:01 +0300

    Add new feature -> DES, AES encryption and decryption

---
+++ DRY/EncryptionAndDecryption/AESAlgorithm.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace DRY.EncryptionAndDecryption
+{
+    public class AESAlgorithm
+    {
+        public static string EncryptData(string plainText, string keyToUse)
+        {
+            byte[] iv = new byte[16];
+            byte[] array;
+
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = Encoding.UTF8.GetBytes(Get128BitString(keyToUse));
+                aes.IV = iv;
+                ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+
+                using (MemoryStream memoryStream = new MemoryStream())
+                {
+                    using (CryptoStream cryptoStream = new CryptoStream(memoryStream, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter streamWriter = new StreamWriter(cryptoStream))
+                        {
+                            streamWriter.Write(plainText);
+                        }
+
+                        array = memoryStream.ToArray();
+                    }
+                }
+            }
+
+            return Convert.ToBase64String(array);
+        }
+
+        public static string DecryptData(string cipherText, string keyToUse)
+        {
+            byte[] iv = new byte[16];
+            byte[] buffer = Convert.FromBase64String(cipherText);
+
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = Encoding.UTF8.GetBytes(Get128BitString(keyToUse));
+                aes.IV = iv;
+                ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+
+                using (MemoryStream memoryStream = new MemoryStream(buffer))
+                {
+                    using (CryptoStream cryptoStream = new CryptoStream(memoryStream, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader streamReader = new StreamReader(cryptoStream))
+                        {
+                            return streamReader.ReadToEnd();
+                        }
+                    }
+                }
+            }
+        }
+
+        public static string Get128BitString(string keyToConvert)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < 16; i++)
+                sb.Append(keyToConvert[i % keyToConvert.Length]);
+            keyToConvert = sb.ToString();
+            return keyToConvert;
+        }
+    }
+}
---
+++ DRY/EncryptionAndDecryption/DESAlgorithm.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace DRY.EncryptionAndDecryption
+{
+    public class DESAlgorithm
+    {
+        public static string EncryptData(string strData, string strEndDcKey)
+        {
+            byte[] key = { };
+            byte[] IV = { 10, 20, 30, 40, 50, 60, 70, 80 };
+            byte[] inputByteArray;
+
+            try
+            {
+                key = Encoding.UTF8.GetBytes(strEndDcKey);
+                DESCryptoServiceProvider objDES = new DESCryptoServiceProvider();
+                inputByteArray = Encoding.UTF8.GetBytes(strData);
+                MemoryStream memoryStream = new MemoryStream();
+                CryptoStream cryptoStream = new CryptoStream(memoryStream, objDES.CreateEncryptor(key, IV), CryptoStreamMode.Write);
+                cryptoStream.Write(inputByteArray, 0, inputByteArray.Length);
+                cryptoStream.FlushFinalBlock();
+                return Convert.ToBase64String(memoryStream.ToArray());
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public static string DecryptData(string strData, string strEndDcKey)
+        {
+            byte[] key = { };
+            byte[] IV = { 10, 20, 30, 40, 50, 60, 70, 80 };
+            byte[] inputByteArray = new byte[strData.Length];
+
+            try
+            {
+                key = Encoding.UTF8.GetBytes(strEndDcKey);
+                DESCryptoServiceProvider objDES = new DESCryptoServiceProvider();
+                inputByteArray = Convert.FromBase64String(strData);
+                MemoryStream memoryStream = new MemoryStream();
+                CryptoStream cryptoStream = new CryptoStream(memoryStream, objDES.CreateDecryptor(key, IV), CryptoStreamMode.Write);
+                cryptoStream.Write(inputByteArray, 0, inputByteArray.Length);
+                cryptoStream.FlushFinalBlock();
+                Encoding encoding = Encoding.UTF8;
+                return encoding.GetString(memoryStream.ToArray());
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
